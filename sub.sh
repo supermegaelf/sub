69aa94a93c4d70317da8ce891471b3e4114a677f
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-02-24 01:05:36 +0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -5,15 +5,21 @@ TEMPLATE_FILE="$TEMPLATE_DIR/index.html"
 CONFIG_FILE="/opt/marzban/.env"
 
 mkdir -p "$TEMPLATE_DIR" &&
-wget -q https://raw.githubusercontent.com/supermegaelf/Marzban-Subscription-Page/main/index.html -O "$TEMPLATE_FILE"
+wget -q https://raw.githubusercontent.com/supermegaelf/Marzban-Subscription-Page/main/index.html -O "$TEMPLATE_FILE" || {
+    echo "Ошибка: не удалось загрузить шаблон"
+    exit 1
+}
 
 if [[ ! -f "$CONFIG_FILE" ]]; then
     echo "Error: файл $CONFIG_FILE не найден"
     exit 1
 fi
 
+cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
+
 sed -i 's/^# *CUSTOM_TEMPLATES_DIRECTORY="\/var\/lib\/marzban\/templates\/"/CUSTOM_TEMPLATES_DIRECTORY="\/var\/lib\/marzban\/templates\/"/' "$CONFIG_FILE"
 sed -i 's/^# *SUBSCRIPTION_PAGE_TEMPLATE="subscription\/index.html"/SUBSCRIPTION_PAGE_TEMPLATE="subscription\/index.html"/' "$CONFIG_FILE"
+sed -i 's/^# *SUB_UPDATE_INTERVAL = "1"/SUB_UPDATE_INTERVAL = "1"/' "$CONFIG_FILE"
 
 read -p "Sub-Site domain: " PRIMARY_DOMAIN
 
