a1406ac92d40c964ada98d932181fe3d8aa78fd5
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-19 00:46:58 +0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -13,7 +13,7 @@ wget -q https://raw.githubusercontent.com/supermegaelf/Marzban-Subscription-Page
 if [[ ! -f "$CONFIG_FILE" ]]; then
     echo "Error: file $CONFIG_FILE not found"
     exit 1
-fi
+}
 
 cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
 
@@ -21,12 +21,12 @@ sed -i 's/^# *CUSTOM_TEMPLATES_DIRECTORY="\/var\/lib\/marzban\/templates\/"/CUST
 sed -i 's/^# *SUBSCRIPTION_PAGE_TEMPLATE="subscription\/index.html"/SUBSCRIPTION_PAGE_TEMPLATE="subscription\/index.html"/' "$CONFIG_FILE"
 sed -i 's/^# *SUB_UPDATE_INTERVAL = "12"/SUB_UPDATE_INTERVAL = "1"/' "$CONFIG_FILE"
 
-read -p "Sub-Site domain: " PRIMARY_DOMAIN
+read -p $'\033[32mSub-Site domain: \033[0m' PRIMARY_DOMAIN
 
 if [ -z "$PRIMARY_DOMAIN" ]; then
     echo "Error: domain cannot be empty"
     exit 1
-fi
+}
 
 sed -i "s|# XRAY_SUBSCRIPTION_URL_PREFIX = \"https://example.com\"|XRAY_SUBSCRIPTION_URL_PREFIX = \"https://$PRIMARY_DOMAIN\"|" "$CONFIG_FILE"
 
